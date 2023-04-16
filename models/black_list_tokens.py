c10396eb6eb00584bcdd598b9b0773609d36238e
sergei <sg_16072002@mail.ru>
2023-04-17 01:00:12 +0800

    test update

--- models/black_list_tokens.py
+++ models/black_list_tokens.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 class BlackListJWT(Base):
     __tablename__ = 'black_list_jwt'
-    REPR_MODEL_NAME = 'использованный токен'
+    REPR_MODEL_NAME = 'использованный jwt токен'
 
     id: Mapped[int] = mapped_column(primary_key=True)
     token: Mapped[str] = mapped_column(index=True)
